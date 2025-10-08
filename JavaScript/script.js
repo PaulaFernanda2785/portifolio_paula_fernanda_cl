d32f667bdf04b45f908c435494220a6aaef564b2
PaulaFernanda2785 <paulafernandacorrealima@gmail.com>
2025-10-08 20:14:53 -0300

    ajustando o main

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,11 @@
 /*=====JS - HEADER-TRANPARENTE=====*/
-
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
-    if(window.scrollY > 100){
-        header.classList.add('transparent');
-    } else {
-        header.classList.remove('transparent');
+    if (header) {
+        if (window.scrollY > 50) {
+            header.classList.add('transparent');
+        } else {
+            header.classList.remove('transparent');
+        }
     }
 });
